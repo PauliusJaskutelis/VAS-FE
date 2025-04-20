87df8100af9b224f9a27eb18f604b3f2ce767377
PauliusJaskutelis <pjaskutelis@gmail.com>
2025-04-20 17:17:52 +0300

    Creating Register page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import theme from './theme/theme';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import ModelStoragePage from './pages/ModelStorage';
+import Register from './pages/Register';
+import OAuthCallback from './pages/OAuthCallback';
 
 function App() {
   return (
@@ -14,8 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           {/* Add other routes here */}
           {/* Example: <Route path="/register" element={<Register />} /> */}
+          <Route path="/oauth2/callback" element={<OAuthCallback />} />
           <Route path="/results" element={<Results />} />
           <Route path="*" element={<Home />} />{' '}
           {/* Redirect to Home for any other route */}
